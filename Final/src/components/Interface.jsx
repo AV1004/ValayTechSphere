fcbb5964ff7765395e9a484bd4d9d824402131b0
Valay Andhariya <andhariyavalay4@gmail.com>
2024-07-07 07:02:17 +0530

    Add features to handle form submission

--- Final/src/components/Interface.jsx
+++ Final/src/components/Interface.jsx
@@ -3,6 +3,7 @@ import { div } from "three/examples/jsm/nodes/Nodes.js";
 import { motion } from "framer-motion";
 import { useAtom } from "jotai";
 import { currentProjectAtom, projects } from "./Projects";
+import { useForm, ValidationError } from "@formspree/react";
 
 const Section = (props) => {
   const { children, mobileTop } = props;
@@ -233,47 +234,74 @@ const ProjectsSection = () => {
 };
 
 const ContactSection = () => {
+  const [state, handleSubmit] = useForm("mdknobky");
+
   return (
     <Section>
       <h2 className="text-3xl md:text-5xl font-bold">Contact me</h2>
       <div className="mt-8 p-8 rounded-md bg-[#1F4E79] bg-opacity-50 md:bg-opacity-100  w-96 max-w-full">
-        <form>
-          <label htmlFor="name" className="font-medium text-white  block mb-1">
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#1F4E79] p-3"
-          />
-          <label
-            htmlFor="email"
-            className="font-medium  text-white block mb-1 mt-8"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#1F4E79]  p-3"
-          />
-          <label
-            htmlFor="email"
-            className="font-medium  text-white block mb-1 mt-8"
-          >
-            Message
-          </label>
-          <textarea
-            name="message"
-            id="message"
-            className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#1F4E79]  p-3"
-          />
-          <button className="bg-[#FF6F3C] text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 ">
-            Submit
-          </button>
-        </form>
+        {state.succeeded ? (
+          <p className="text-white text-center">Thank you for your message!</p>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <label
+              htmlFor="name"
+              className="font-medium text-white  block mb-1"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#1F4E79] p-3"
+              required
+            />
+            <ValidationError
+              className="mt-1 text-white"
+              errors={state.errors}
+            />
+            <label
+              htmlFor="email"
+              className="font-medium  text-white block mb-1 mt-8"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#1F4E79]  p-3"
+              required
+            />
+            <ValidationError
+              className="mt-1 text-white"
+              errors={state.errors}
+            />
+            <label
+              htmlFor="email"
+              className="font-medium  text-white block mb-1 mt-8"
+            >
+              Message
+            </label>
+            <textarea
+              name="message"
+              id="message"
+              className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#1F4E79]  p-3"
+              required
+            />
+            <ValidationError
+              className="mt-1 text-white"
+              errors={state.errors}
+            />
+            <button
+              className="bg-[#FF6F3C] text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 "
+              disabled={state.submitting}
+            >
+              Submit
+            </button>
+          </form>
+        )}
       </div>
     </Section>
   );
