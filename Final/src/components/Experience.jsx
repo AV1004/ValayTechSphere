94c0d2e41c189f52af186b67bb0615d779aacdc9
Valay Andhariya <andhariyavalay4@gmail.com>
2024-07-07 06:00:29 +0530

    Make the Normal HTMl and 3D both resposive

--- Final/src/components/Experience.jsx
+++ Final/src/components/Experience.jsx
@@ -20,7 +20,10 @@ export const Experience = ({ menuOpened }) => {
   const { viewport } = useThree();
   const data = useScroll();
 
-  console.log(viewport.width, viewport.height);
+  // console.log(viewport.width, viewport.height);
+  const isMoblie = window.innerWidth < 768;
+  const responsiveRatio = viewport.width / 12;
+  const officeScaleRatio = Math.max(0.5, Math.min(0.9 * responsiveRatio, 0.9));
 
   const [section, setSection] = useState(0);
 
@@ -46,6 +49,7 @@ export const Experience = ({ menuOpened }) => {
   }, [section]);
 
   const characterContainerAboutRef = useRef();
+  const characterGroup = useRef();
 
   useFrame((state) => {
     let curSection = Math.floor(data.scroll.current * data.pages);
@@ -62,7 +66,11 @@ export const Experience = ({ menuOpened }) => {
     state.camera.lookAt(cameraLookAtX.get(), 0, 0);
 
     // const postion = new THREE.Vector3();
-    // characterContainerAboutRef.current.getWorldPosition(postion);
+    if (section === 0) {
+      characterContainerAboutRef.current.getWorldPosition(
+        characterGroup.current.position
+      );
+    }
     // console.log([postion.x, postion.y, postion.z]);
 
     // const quaternion = new THREE.Quaternion();
@@ -76,33 +84,40 @@ export const Experience = ({ menuOpened }) => {
       {/* <OrbitControls /> */}
       <Background />
       <motion.group
-        position={[1.945477272147525, 0.09000000000000001, 2.4272435072388965]}
+        ref={characterGroup}
         rotation={[-3.141592653589793, 1.2053981633974482, 3.141592653589793]}
+        scale={[officeScaleRatio, officeScaleRatio, officeScaleRatio]}
         animate={"" + section}
         transition={{
           duration: 0.8,
         }}
         variants={{
           0: {
-            scaleX: 0.9,
-            scaleY: 0.9,
-            scaleZ: 0.9,
+            scaleX: officeScaleRatio,
+            scaleY: officeScaleRatio,
+            scaleZ: officeScaleRatio,
           },
           1: {
             y: -viewport.height + 0.5,
-            x: 0,
+            x: isMoblie ? 0.3 : 0,
             z: 6.5,
             rotateX: 0,
-            rotateY: 0,
+            rotateY: isMoblie ? -Math.PI / 2 : 0,
             rotateZ: 0,
+            scaleX: isMoblie ? 1.1 : 1,
+            scaleY: isMoblie ? 1.1 : 1,
+            scaleZ: isMoblie ? 1.1 : 1,
           },
           2: {
-            x: -2,
+            x: isMoblie ? -1.4 : -2,
             y: -viewport.height * 2 + -0.9,
             z: 0,
             rotateX: 0,
             rotateY: Math.PI / 2,
             rotateZ: 0,
+            scaleX: 1,
+            scaleY: 1,
+            scaleZ: 1,
           },
           3: {
             y: -viewport.height * 3 + 0.6,
@@ -111,6 +126,9 @@ export const Experience = ({ menuOpened }) => {
             rotateX: 0,
             rotateY: -Math.PI / 4,
             rotateZ: 0,
+            scaleX: 1,
+            scaleY: 1,
+            scaleZ: 1,
           },
         }}
       >
@@ -118,11 +136,15 @@ export const Experience = ({ menuOpened }) => {
       </motion.group>
       <ambientLight intensity={1} />
       <motion.group
-        position={[1.5, 2, 3]}
-        scale={[0.9, 0.9, 0.9]}
+        position={[
+          isMoblie ? 0 : 1.5 * officeScaleRatio,
+          isMoblie ? -viewport.height / 6 : 2,
+          3,
+        ]}
+        scale={[officeScaleRatio, officeScaleRatio, officeScaleRatio]}
         rotation-y={-Math.PI / 4}
         animate={{
-          y: section === 0 ? 0 : -1,
+          y: isMoblie ? -viewport.height / 6 : 0,
         }}
       >
         <Office section={section} />
@@ -134,12 +156,22 @@ export const Experience = ({ menuOpened }) => {
           {/* <Avatar animation={section === 0 ? "Typing" : "Standing"} /> */}
         </group>
       </motion.group>
-      SKILLS
+
+      {/* SKILLS */}
       <motion.group
-        position={[0, -1.5, -11]}
+        position={[
+          0,
+          isMoblie ? -viewport.height : -1.5 * officeScaleRatio,
+          -11,
+        ]}
         animate={{
           z: section === 1 ? 0 : -10,
-          y: section === 1 ? -viewport.height : -1.5,
+          y:
+            section === 1
+              ? -viewport.height
+              : isMoblie
+              ? -viewport.height
+              : -1.5 * officeScaleRatio,
         }}
       >
         <directionalLight position={[-5, 3, 5]} intensity={0.4} />
